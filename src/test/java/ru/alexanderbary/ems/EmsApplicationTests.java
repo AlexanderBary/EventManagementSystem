8d5c47a57f3d7cf284e49c5bcb7bbce12aa11ec6
Alexander <alexbary98@gmail.com>
2023-07-15 20:30:29 +0300

    Initial commit

---
+++ src/test/java/ru/alexanderbary/ems/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexanderbary.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
